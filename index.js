05537fd75a47d076e7eb99183dae0b57258dc69a
cakmakg <cakmak4834@gmail.com>
2025-08-13 00:24:29 +0200

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express = require('express');
 const app = express();
 
 require("dotenv").config();
-const PORT = process.env?.PORT || 8000;
+const PORT = process.env?.PORT || 5000;
 
 // AsyncErrors to errorHandler:
 require('express-async-errors');
