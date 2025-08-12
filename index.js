3927144c2885a5f6c7563c10ca8d52b6c45cf73a
cakmakg <cakmak4834@gmail.com>
2025-08-13 00:26:04 +0200

    index route update

--- index.js
+++ index.js
@@ -112,9 +112,9 @@ app.all('/', (req, res) => {
         // session: req.session
         user: req.user,
         documents: {
-            swagger:'http://127.0.0.1:8000/documents/swagger',
-            redoc:'http://127.0.0.1:8000/documents/redoc',
-            json:'http://127.0.0.1:8000/documents/json',
+            swagger:'http://127.0.0.1:5000/documents/swagger',
+            redoc:'http://127.0.0.1:5000/documents/redoc',
+            json:'http://127.0.0.1:5000/documents/json',
         }
     })
 });
